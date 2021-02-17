9f5a91bf6c0b4f20dcc5e16abb96818ceeb4e1f6
송남규 <@DESKTOP-QKHS66J>
2021-02-17 15:35:39 +0900

    메인페이지에서 고객센터 화면전환

--- src/com/jihunh/jsp/customerservice/controller/customerservice.java
+++ src/com/jihunh/jsp/customerservice/controller/customerservice.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/customer")
+@WebServlet("/customer/list")
 public class customerservice extends HttpServlet {
   
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
