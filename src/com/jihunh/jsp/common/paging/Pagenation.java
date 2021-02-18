2299b2a77baf3b3b5990aaa64b1886b8cfbe514e
JihunHan <dcmlefhhjh@gmail.com>
2021-02-19 00:18:16 +0900

    02/19(금) 지훈 페이지 처리

--- src/com/jihunh/jsp/common/paging/Pagenation.java
+++ src/com/jihunh/jsp/common/paging/Pagenation.java
@@ -1,10 +1,12 @@
 package com.jihunh.jsp.common.paging;
 
 
+import com.jihunh.jsp.admin.model.dto.NoticePageInfoDTO;
 import com.jihunh.jsp.customerservice.model.dto.PageInfoDTO;
 
 public class Pagenation {
 
+	/* 이걸 기본 베이스로 페이지네이션으로 사용 권장함 */
 	public static PageInfoDTO getPageInfo(int pageNo, int totalCount, int limit, int buttonAmount) {
 	
 	int maxPage;
@@ -32,6 +34,36 @@ public class Pagenation {
 	endRow = startRow + limit -1;
 	
 	return new PageInfoDTO(pageNo, totalCount, limit, buttonAmount, maxPage, startPage, endPage, startRow, endRow);
+	}
+	
+	/* 지훈쓰 전용 페이징처리 메소드. 앞에 DTO 변경 금지 */
+	public static NoticePageInfoDTO getPageInfoNotice(int pageNo, int totalCount, int limit, int buttonAmount) {
+		
+		int maxPage;
+		int startPage;
+		int endPage;
+		int startRow;
+		int endRow;
+		
+		maxPage = (int) Math.ceil((double) totalCount / limit);
+		
+		startPage = (int) (Math.ceil((double) pageNo / buttonAmount) -1) * buttonAmount + 1;
+		
+		endPage = startPage + buttonAmount -1;
+		
+		if(maxPage < endPage) {
+			endPage = maxPage;
+		}
+		
+		if(maxPage == 0 && endPage == 0) {
+			maxPage = startPage;
+			endPage = startPage;
+		}
+		
+		startRow = (pageNo - 1) * limit + 1;
+		endRow = startRow + limit -1;
+		
+		return new NoticePageInfoDTO(pageNo, totalCount, limit, buttonAmount, maxPage, startPage, endPage, startRow, endRow);
 	}
 }
 
