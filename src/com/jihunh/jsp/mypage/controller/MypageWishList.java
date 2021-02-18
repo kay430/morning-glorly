12251b1ae438a523cd20a8c46a5d86531cf5e39c
user <user@DESKTOP-H8JS3HO>
2021-02-18 12:40:09 +0900

    태헌- 뷰페이지 서블릿 추가

---
+++ src/com/jihunh/jsp/mypage/controller/MypageWishList.java
@@ -0,0 +1,25 @@
+package com.jihunh.jsp.mypage.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/mypage/wishList")
+public class MypageWishList extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String path = "/WEB-INF/views/common/wishList.jsp";
+		request.getRequestDispatcher(path).forward(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
