e44f7a283ef91df9d2f90920b6dbc6182481ff6e
최지환 <@DESKTOP-9IV2C8H>
2021-02-19 12:31:41 +0900

    질문게시판 페이징처리 구현중

--- src/com/jihunh/jsp/common/paging/Pagenation.java
+++ src/com/jihunh/jsp/common/paging/Pagenation.java
@@ -3,6 +3,7 @@ package com.jihunh.jsp.common.paging;
 
 import com.jihunh.jsp.admin.model.dto.NoticePageInfoDTO;
 import com.jihunh.jsp.customerservice.model.dto.PageInfoDTO;
+import com.jihunh.jsp.question.model.dto.QuestionPageInfoDTO;
 
 public class Pagenation {
 
@@ -64,6 +65,36 @@ public class Pagenation {
 		endRow = startRow + limit -1;
 		
 		return new NoticePageInfoDTO(pageNo, totalCount, limit, buttonAmount, maxPage, startPage, endPage, startRow, endRow);
+	}
+	
+		/*지환쓰 전용 페이징처리 메소드 내꺼임 */
+        public static QuestionPageInfoDTO getPageInfoQuestion(int pageNo, int totalCount, int limit, int buttonAmount) {
+		
+		int maxPage;
+		int startPage;
+		int endPage;
+		int startRow;
+		int endRow;
+		
+		maxPage = (int) Math.ceil((double) totalCount / limit);
+		
+		startPage = (int) (Math.ceil((double) pageNo / buttonAmount) -1) * buttonAmount + 1;
+		
+		endPage = startPage + buttonAmount -1;
+		
+		if(maxPage < endPage) {
+			endPage = maxPage;
+		}
+		
+		if(maxPage == 0 && endPage == 0) {
+			maxPage = startPage;
+			endPage = startPage;
+		}
+		
+		startRow = (pageNo - 1) * limit + 1;
+		endRow = startRow + limit -1;
+		
+		return new QuestionPageInfoDTO(pageNo, totalCount, limit, buttonAmount, maxPage, startPage, endPage, startRow, endRow);
 	}
 }
 
--- src/com/jihunh/jsp/question/model/dao/QuestionDAO.java
+++ src/com/jihunh/jsp/question/model/dao/QuestionDAO.java
@@ -1,8 +1,12 @@
 package com.jihunh.jsp.question.model.dao;
 
+import static com.greedy.jsp.common.jdbc.JDBCTemplate.close;
+import static com.jihunh.jsp.common.jdbc.JDBCTemplate.close;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -10,11 +14,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+import com.greedy.jsp.board.model.dto.BoardDTO;
+import com.greedy.jsp.board.model.dto.CategoryDTO;
+import com.greedy.jsp.board.model.dto.PageInfoDTO;
+import com.greedy.jsp.member.model.dto.MemberDTO;
 import com.jihunh.jsp.common.config.ConfigLocation;
 import com.jihunh.jsp.member.model.dto.MgDTO;
 import com.jihunh.jsp.question.model.dto.QuestionDTO;
-
-import static com.jihunh.jsp.common.jdbc.JDBCTemplate.close;
+import com.jihunh.jsp.question.model.dto.QuestionPageInfoDTO;
 
 public class QuestionDAO {
 	
@@ -29,7 +36,7 @@ public class QuestionDAO {
 		}
 				
 	}
-	public List<QuestionDTO> selectAllQuestionService(Connection con) {
+	public List<QuestionDTO> selectAllQuestionService(Connection con, QuestionPageInfoDTO QuestionPageInfo) {
 		
 		Statement stmt = null;
 		ResultSet rset = null;
@@ -55,6 +62,7 @@ public class QuestionDAO {
 				question.getWriter().setId(rset.getString("MEMBER_ID"));
 				question.setCount(rset.getInt("QNA_COUNT"));
 				question.setCreateDate(rset.getDate("CREATED_DATE"));
+				question.setStatus(rset.getString("QNA_STATUS"));
 				
 				questionList.add(question);
 				
@@ -68,5 +76,82 @@ public class QuestionDAO {
 		}
 		return questionList;
 	}
+	public int selectTotalCount(Connection con) {
+		
+		Statement stmt = null;
+		ResultSet rset = null;
+		
+		int totalCount = 0;
+		
+		String query = prop.getProperty("selectTotalCount");
+		
+		try {
+			stmt = con.createStatement();
+			rset = stmt.executeQuery(query);
+			
+			if(rset.next()) {
+				totalCount = rset.getInt("COUNT(*)");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(stmt);
+		}
+		
+		return totalCount;
+	}
+
+	
+	
+	
+	
+/*public List<BoardDTO> selectBoardList(Connection con, PageInfoDTO pageInfo) {
+		
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		
+		List<BoardDTO> boardList = null;
+		
+		String query = prop.getProperty("selectBoardList");
+		
+		try {
+			pstmt = con.prepareStatement(query);
+			pstmt.setInt(1, pageInfo.getStartRow());
+			pstmt.setInt(2, pageInfo.getEndRow());
+			
+			rset = pstmt.executeQuery();
+			
+			boardList = new ArrayList<>();
+			
+			while(rset.next()) {
+				BoardDTO board = new BoardDTO();
+				board.setCategory(new CategoryDTO());
+				board.setWriter(new MemberDTO());
+				
+				board.setNo(rset.getInt("BOARD_NO"));
+				board.setType(rset.getInt("BOARD_TYPE"));
+				board.setCategoryCode(rset.getInt("CATEGORY_CODE"));
+				board.getCategory().setName(rset.getString("CATEGORY_NAME"));
+				board.setTitle(rset.getString("BOARD_TITLE"));
+				board.setBody(rset.getString("BOARD_BODY"));
+				board.setWriterMemberNo(rset.getInt("BOARD_WRITER_MEMBER_NO"));
+				board.getWriter().setNickname(rset.getString("NICKNAME"));
+				board.setCount(rset.getInt("BOARD_COUNT"));
+				board.setCreatedDate(rset.getDate("CREATED_DATE"));
+				board.setStatus(rset.getString("BOARD_STATUS"));
+				
+				boardList.add(board);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+		
+		return boardList;
+	}*/
 
 }
