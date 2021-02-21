1d6633a11ccbce8ea14b6934a24eadbebd5e6a34
user <user@DESKTOP-H8JS3HO>
2021-02-21 18:30:37 +0900

    리뷰페이지 5류 힘들때  힘든게 5류다 ...

--- src/com/jihunh/jsp/common/paging/Pagenation.java
+++ src/com/jihunh/jsp/common/paging/Pagenation.java
@@ -4,6 +4,7 @@ package com.jihunh.jsp.common.paging;
 import com.jihunh.jsp.admin.model.dto.NoticePageInfoDTO;
 import com.jihunh.jsp.customerservice.model.dto.PageInfoDTO;
 import com.jihunh.jsp.question.model.dto.QuestionPageInfoDTO;
+import com.jihunh.jsp.review.dto.ReviewPageInfoDTO;
 
 public class Pagenation {
 
@@ -96,6 +97,35 @@ public class Pagenation {
 		
 		return new QuestionPageInfoDTO(pageNo, totalCount, limit, buttonAmount, maxPage, startPage, endPage, startRow, endRow);
 	}
+        public static ReviewPageInfoDTO getPageInfoReview(int pageNo, int totalCount, int limit, int buttonAmount) {
+    		
+    		int maxPage;
+    		int startPage;
+    		int endPage;
+    		int startRow;
+    		int endRow;
+    		
+    		maxPage = (int) Math.ceil((double) totalCount / limit);
+    		
+    		startPage = (int) (Math.ceil((double) pageNo / buttonAmount) -1) * buttonAmount + 1;
+    		
+    		endPage = startPage + buttonAmount -1;
+    		
+    		if(maxPage < endPage) {
+    			endPage = maxPage;
+    		}
+    		
+    		if(maxPage == 0 && endPage == 0) {
+    			maxPage = startPage;
+    			endPage = startPage;
+    		}
+    		
+    		startRow = (pageNo - 1) * limit + 1;
+    		endRow = startRow + limit -1;
+    		
+    		return new ReviewPageInfoDTO(pageNo, totalCount, limit, buttonAmount, maxPage, startPage, endPage, startRow, endRow);
+    	}
+
 }
 
 
